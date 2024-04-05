db4d35cef7fd82a89ca4ab8bd549ed949c435dd3
Woswoswald <116073615+Woswoswald@users.noreply.github.com>
2024-04-05 16:18:25 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
+from unittest import TestCase, main 
+from pathlib import Path
+import os
 
+import streamlit as st 
+from ultralytics import YOLO
+from sqlalchemy import create_engine
+from sqlalchemy.engine.base import Engine
+
+import database
+import settings
+
+class TestFiles(TestCase):
+    # Vérifie si ces fichiers sont dans le dossier racine
+    def test_root_files(self):
+        root_dir_files = os.listdir()
+        self.assertIn("streamlit_app.py", root_dir_files)
+
+
+if __name__  == '__main__':
+    main(verbosity=2)
